d295eed4f36a12ee5f37a4230acfa6c8170fcd5e
Piotr Muszel <pmuszel@gmail.com>
2020-11-18 22:26:17 +0100

    Init commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/product', (req, res, next) => {
+    
+});
+
+app.use('/', (req, res, next) => {
+    
+});
+
+app.listen(3000);
