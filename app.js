f4e34a8bb2bdbc506fc408b82b907c152e7e83b6
unknown <p.muszel@infosolutions.local>
2020-11-19 11:10:31 +0100

    Added router

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body.title);
-    res.redirect('/');
-});
-
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
-});
 
 app.listen(3000);
