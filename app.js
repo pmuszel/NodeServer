4396b08bbc2b307a2c267cd3b30505a2e76e9e44
Piotr Muszel <pmuszel@gmail.com>
2020-11-19 14:32:53 +0100

    Serving static html files

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const rootDir = require('./util/path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -8,11 +12,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send("<h1>Page not found!</h1>")
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 
